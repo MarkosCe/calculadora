817ac86deb8b70b7af98063e23cd6eb64381aacd
MarkosCe <cesar.markos99@gmail.com>
2022-07-07 18:26:01 -0500

    initial commit-diseño web

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './componentes/Boton';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className="logo-contenedor">
+        <span>C</span>
+        <span>A</span>
+        <span>L</span>
+        <span>C</span>
+        <span>U</span>
+        <span>L</span>
+        <span>A</span>
+        <span>D</span>
+        <span>O</span>
+        <span>R</span>
+        <span>A</span>
+      </div>
+      <div className='calculadora-contenedor'>
+        <div className='fila'>
+          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>+</Boton>
+          <Boton>.</Boton>
+        </div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+      </div>
     </div>
   );
 }
